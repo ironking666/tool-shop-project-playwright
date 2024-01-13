5495dccbeb9ad21c554ecde3d7667499043d9021
Krzysztof Waliszewski <krzysztof.waliszewski87@gmail.com>
2024-01-13 14:42:21 +0100

    test: cart test refactor

--- tests/cart-tests.spec.ts
+++ tests/cart-tests.spec.ts
@@ -1,33 +1,31 @@
 import { HomePage } from '../src/pages/home.page';
 import { expect, test } from '@playwright/test';
 
-test.describe('Verify cart', () => {});
-test('user can add product to cart @REQ-04-01', async ({ page }) => {
-  const homePage = new HomePage(page);
-  await homePage.goto();
-  await homePage.addProductToCart();
+test.describe('Verify cart', () => {
+  let homePage: HomePage;
+  test.beforeEach(async ({ page }) => {
+    homePage = new HomePage(page);
+    await homePage.goto();
+  });
+  test('user can add product to cart @REQ-04-01', async ({}) => {
+    await homePage.addProductToCart();
 
-  await expect(homePage.expectedMessage).toBeVisible();
-});
+    await expect(homePage.expectedMessage).toBeVisible();
+  });
 
-test('user can change product quantity in cart @REQ-04-02', async ({
-  page,
-}) => {
-  const homePage = new HomePage(page);
-  await homePage.goto();
-  await homePage.addProductToCart();
-  await homePage.navigateToCart();
-  await homePage.changeQuantityInCart();
+  test('user can change product quantity in cart @REQ-04-02', async ({}) => {
+    await homePage.addProductToCart();
+    await homePage.navigateToCart();
+    await homePage.changeQuantityInCart();
 
-  expect(parseInt(await homePage.quantity.inputValue())).toBeGreaterThan(1);
-});
+    expect(parseInt(await homePage.quantity.inputValue())).toBeGreaterThan(1);
+  });
 
-test('user can delete product from cart @REQ-04-02', async ({ page }) => {
-  const homePage = new HomePage(page);
-  await homePage.goto();
-  await homePage.addProductToCart();
-  await homePage.navigateToCart();
-  await homePage.removeProductFromCart();
+  test('user can delete product from cart @REQ-04-02', async ({}) => {
+    await homePage.addProductToCart();
+    await homePage.navigateToCart();
+    await homePage.removeProductFromCart();
 
-  await expect(homePage.priceInCart).toHaveText('$0.00');
+    await expect(homePage.priceInCart).toHaveText('$0.00');
+  });
 });
