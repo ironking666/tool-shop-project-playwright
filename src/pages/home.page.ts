2512f0b61099c92c3c9602eb5f42a7ef10320870
Krzysztof Waliszewski <krzysztof.waliszewski87@gmail.com>
2024-02-13 14:02:50 +0100

    test: search functionality tests

--- src/pages/home.page.ts
+++ src/pages/home.page.ts
@@ -3,6 +3,9 @@ import { Page } from 'playwright/test';
 export class HomePage {
   sortSelect = this.page.locator('[data-test="sort"]');
   sortedProduct = this.page.locator('h5.card-title').nth(0);
+  searchInput = this.page.locator('input[data-test="search-query"]');
+  searchButton = this.page.locator('button[data-test="search-submit"]');
+  searchReset = this.page.locator('button[data-test="search-reset"]');
   constructor(private page: Page) {}
 
   async goto(): Promise<void> {
---
+++ tests/search-functionality.spec.ts
@@ -0,0 +1,24 @@
+import { HomePage } from '../src/pages/home.page';
+import { expect, test } from '@playwright/test';
+
+test.describe('Verifying search functionality', () => {
+  let homePage: HomePage;
+  test.beforeEach(async ({ page }) => {
+    let expectedText = 'Square Ruler';
+    homePage = new HomePage(page);
+    await homePage.goto();
+  });
+
+  test('user can search by search area @REQ-09-01', async () => {
+    const expectedText = 'Square Ruler';
+    await homePage.searchInput.fill('Square Ruler');
+    await homePage.searchButton.click();
+    await expect(homePage.sortedProduct).toHaveText(expectedText);
+  });
+  test('user can clear search area @REQ-09-02', async () => {
+    const expectedText = 'Square Ruler';
+    await homePage.searchInput.fill('Square Ruler');
+    await homePage.searchReset.click();
+    await expect(homePage.sortedProduct).not.toHaveText(expectedText);
+  });
+});
