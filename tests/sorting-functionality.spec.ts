76591eec8a91dd234c20aa55310a2d6a0a74eb90
Krzysztof Waliszewski <krzysztof.waliszewski87@gmail.com>
2024-02-13 12:09:46 +0100

    test: sort functionality refactor

--- tests/sorting-functionality.spec.ts
+++ tests/sorting-functionality.spec.ts
@@ -8,25 +8,25 @@ test.describe('Verifying sort functionality', () => {
     await homePage.goto();
   });
 
-  test('user can sort by Name(A-Z) @REQ-08-01', async ({ page }) => {
+  test('user can sort by Name(A-Z) @REQ-08-01', async () => {
     const expectedText = ' Adjustable Wrench ';
     await homePage.sortSelect.selectOption('name,asc');
     await expect.soft(homePage.sortedProduct).toBeVisible();
     await expect(homePage.sortedProduct).toHaveText(expectedText);
   });
-  test('user can sort by Name(Z-A) @REQ-08-02', async ({ page }) => {
+  test('user can sort by Name(Z-A) @REQ-08-02', async () => {
     const expectedText = ' Wood Saw ';
     await homePage.sortSelect.selectOption('name,desc');
     await expect.soft(homePage.sortedProduct).toBeVisible();
     await expect(homePage.sortedProduct).toHaveText(expectedText);
   });
-  test('user can sort by Price(High - Low) @REQ-08-03', async ({ page }) => {
+  test('user can sort by Price(High - Low) @REQ-08-03', async () => {
     const expectedText = ' Drawer Tool Cabinet ';
     await homePage.sortSelect.selectOption('price,desc');
     await expect.soft(homePage.sortedProduct).toBeVisible();
     await expect(homePage.sortedProduct).toHaveText(expectedText);
   });
-  test('user can sort by Price(Low - High) @REQ-08-04', async ({ page }) => {
+  test('user can sort by Price(Low - High) @REQ-08-04', async () => {
     const expectedText = ' Washers ';
     await homePage.sortSelect.selectOption('price,asc');
     await expect.soft(homePage.sortedProduct).toBeVisible();
